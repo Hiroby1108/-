0fe0728b4ba58b68ae5374d2385ca4f14c2bb7c5
Wakis <s1723085@cce.kanagawa-it.ac.jp>
2019-02-08 19:45:00 +0900

    Add GameRule and any

--- Assets/Wakis/GameRule.cs
+++ Assets/Wakis/GameRule.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-#if UNIUNITY_EDITOR
-
-#endif
+using UnityEngine.SceneManagement;
 
 public class GameRule : MonoBehaviour
 {
@@ -17,10 +15,15 @@ public class GameRule : MonoBehaviour
     private STEP step;              // 今のSTEP.
     private STEP next_step;      // 次のSTEP.
     private float step_timer;     // 経過時間を入れる.
-    private PlayerController player;	// playerと連携.
+    private PlayerController player;    // playerと連携.
+
+
+    [SerializeField, Header("次のシーン名"),Tooltip("未設定でもれなくワキルーム行")]
+    private string NextSceneName;
 
     [SerializeField, Header("制限時間")]
     private float limitTime;
+    
     
     private ItemGenerat IG;
     private bool OneLoad;
@@ -34,14 +37,46 @@ public class GameRule : MonoBehaviour
         IG = GameObject.Find("ItemGenerator").GetComponent<ItemGenerat>();
         n_Trash = IG.NumOfTrash;
         n_Living = IG.NumOfLiving;
-        //player = GameObject.Find("ItemGenerator").GetComponent<PlayerController>();
+        player = GameObject.Find("Player").GetComponent<PlayerController>();
+        next_step = STEP.SET;    // 最初はSETから.
 
         OneLoad = true;
     }
 
     // Update is called once per frame
+
+
     void Update()
     {
+        step_timer += Time.deltaTime;    // 経過時間を取得.
+
+        // （１）ステップ変化時のみ.
+        if (next_step != STEP.NONE)
+        {
+            step = next_step;       // 現状に反映.
+            next_step = STEP.NONE;  // 変化待ち.
+            step_timer = 0.0f;      // 経過時間リセット.
+
+            switch (step)
+            {
+                case STEP.SET:
+                    next_step = STEP.PLAY;  // 次はPLAYステップに.
+                    break;
+            }
+        }
+
+        // （２）繰り返し実行.
+        switch (step)
+        {
+            case STEP.PLAY://Player操作
+                if (limitTime < step_timer) next_step = STEP.FINISH;
+                break;
+            case STEP.FINISH:
+                SceneManager.LoadScene(NextSceneName);
+                break;
+        }
 
     }
+
+
 }
