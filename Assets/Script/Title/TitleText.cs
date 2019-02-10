c80b74f3b0248c78e6170f5a070073189834cc79
Hiroby1108 <hirobymiya@gmail.com>
2019-02-10 17:28:32 +0900

    title

---
+++ Assets/Script/Title/TitleText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleText : MonoBehaviour
+{
+    [SerializeField]
+    private Button StartButton;
+    // Start is called before the first frame update
+    public void OnStartButtonClicked()
+    {
+        SceneManager.LoadScene("tutorial");
+    }
+}
