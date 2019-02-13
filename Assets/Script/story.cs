c1c5349305b5d2822c5bd9972ca784146bef3b0b
Hiroby1108 <hirobymiya@gmail.com>
2019-02-13 20:28:35 +0900

    ルートの元、イベント提起

---
+++ Assets/Script/story.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class story : MonoBehaviour
+{
+         Text Story;
+         Text Score;
+   public GameObject rute;
+    void Start()
+    {
+        Story = GameObject.Find("Story").GetComponentInChildren<Text>();
+        Score = GameObject.Find("Score").GetComponentInChildren<Text>();
+        Story.GetComponent<Text>().enabled = false;
+    }
+
+    void Update()
+    {
+        Invoke("call", 2.0f);
+    }
+    void call()
+    {
+        
+        Story.GetComponent<Text>().enabled = true;
+        gameObject.SetActive(true);
+    }
+}
