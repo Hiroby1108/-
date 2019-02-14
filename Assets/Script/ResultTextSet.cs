3994349d2f77af3ab2b0b9e5db25ec8cac82a24a
Inomaruti <22inomata.kouki@gmail.com>
2019-02-14 15:49:59 +0900

    リザルトスクリプトコメント付

--- Assets/Script/ResultTextSet.cs
+++ Assets/Script/ResultTextSet.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class ResultTextSet : MonoBehaviour{
     GameObject ResultUI_1;
-    /*GameObject ResultUI_1_1;
+    /*GameObject ResultUI_1_1;  //修正用
     GameObject ResultUI_1_2;
     GameObject ResultUI_1_3;*/
     GameObject ResultUI_2;
@@ -14,7 +14,7 @@ public class ResultTextSet : MonoBehaviour{
     private int SceneCount;
     void Start(){
         ResultUI_1 = GameObject.Find("ResultText1");
-        /*ResultUI_1_1 = GameObject.Find("ResultText1_1");
+        /*ResultUI_1_1 = GameObject.Find("ResultText1_1");  //修正用
         ResultUI_1_2 = GameObject.Find("ResultText1_2");
         ResultUI_1_3 = GameObject.Find("ResultText1_3");*/
         //ResultUI_2 = GameObject.Find("ResultText2");
@@ -40,14 +40,13 @@ public class ResultTextSet : MonoBehaviour{
 
     private void TextTransparence_1(){    //最初のリザルト画面(回収したごみの表示テキスト）の非表示
         Destroy(ResultUI_1);
-        //ResultUI_1.GetComponent<Text>().enabled = false;
+        //ResultUI_1.GetComponent<Text>().enabled = false;  //修正用（テキストを個別非表示させたい場合）
         //ResultUI_1_1.GetComponent<Text>().enabled = false;
         //ResultUI_1_2.GetComponent<Text>().enabled = false;
         //ResultUI_1_3.GetComponent<Text>().enabled = false;
     }
 
-    private void TextDisplay_2(){
+    private void TextDisplay_2(){   //リザルト画面その2のÜI表示
         ResultUI_2.GetComponent<Canvas>().enabled = true;
-        //ResultUI_22.GetComponent<Text>().enabled = true;
     }
 }
