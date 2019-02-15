373fdc295f2558879ff2911331624b88a6981714
Inomaruti <22inomata.kouki@gmail.com>
2019-02-15 11:33:22 +0900

    リザルトに海の追加、テキスト追加＆調整、表示時間調整、タイムアタックシーンのscript追加（実装はしていない）

---
+++ Assets/Script/story.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class story : MonoBehaviour
+{
+    bool a=false;
+    Text Story;
+   public GameObject rute;
+
+    void Start()
+    {
+        Story = GameObject.Find("Story").GetComponentInChildren<Text>();
+
+        Story.GetComponent<Text>().enabled = false;
+        rute.gameObject.SetActive(false);
+        Invoke("call", 4.0f);//ここの秒数をいじるとイベント発生時間が変わる
+    }
+
+    void Update()
+    {
+    }
+    void call()
+    {
+
+        Story.GetComponent<Text>().enabled = true;
+        rute.gameObject.SetActive(true);
+        Destroy(Story, 6);//文字消す
+        a = true;
+    }
+}
