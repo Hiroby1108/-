6f532aea4792593a0e6685b28a097afecc8f3550
Inomaruti <22inomata.kouki@gmail.com>
2019-02-15 19:48:47 +0900

    タイムアタックにイルカ、タイマー追加など

--- Assets/Script/story.cs
+++ Assets/Script/story.cs
@@ -16,7 +16,7 @@ public class story : MonoBehaviour
 
         Story.GetComponent<Text>().enabled = false;
         rute.gameObject.SetActive(false);
-        Invoke("call", 2.0f);//ここの秒数をいじるとイベント発生時間が変わる
+        Invoke("call", 15.0f);//ここの秒数をいじるとイベント発生時間が変わる
     }
 
     void Update()
