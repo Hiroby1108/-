08e04872896be5ee69844660703799863a4e975e
Inomaruti <22inomata.kouki@gmail.com>
2019-02-15 12:05:18 +0900

    いったんscript戻し

--- Assets/Script/EventSystem.cs
+++ Assets/Script/EventSystem.cs
@@ -6,17 +6,10 @@ public class EventSystem : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField, Header("ゴミリスト"), Tooltip("Assets/Prefab/Trash01～03 :を使用\n\"Rigidbody\"を適用したものを使用")]
-    List<Rigidbody> trash = new List<Rigidbody>();
+    List<Rigidbody> trashRig = new List<Rigidbody>();
 
     [SerializeField, Header("生物リスト"), Tooltip("まだ使用物を決めていない\n\"Rigidbody\"を適用したものを使用")]
-    List<Rigidbody> living = new List<Rigidbody>();
-
-    [SerializeField, Header("ゴミ生成個数")]
-    int numOfEventTrash = 10;
-    public int NumOfEventTrash{ get { return numOfEventTrash; } }
-    [SerializeField, Header("生物生成個数")]
-    int numOfEventLiving = 10;
-    public int NumOfEventLiving { get { return numOfEventLiving; } }
+    List<Rigidbody> livingRig = new List<Rigidbody>();
 
     [SerializeField, Header("イベントx座標")]
     int X = 10;
@@ -37,7 +30,7 @@ public class EventSystem : MonoBehaviour
     int Zs = 10;
     public int zs { get { return Zs; } }
 
-
+   
     public GameObject rute;
 
     bool a = false;
@@ -48,27 +41,13 @@ public class EventSystem : MonoBehaviour
 
     void Update()
     {
-        if (!a)
-            if (rute.gameObject.activeSelf == true)
-        {
-            gameObject.transform.position = new Vector3(x, y, z);
-            gameObject.transform.localScale = new Vector3(xs, ys, Zs);
-            Appearance(numOfEventTrash, trash);//ゴミ生成
-            Appearance(numOfEventLiving, living);//生き物生成
-            a = true;
-        }
+        Invoke("call", 4.0f);
     }
-    private void Appearance(int i_num, List<Rigidbody> list)
+    void call()
     {
-        if (list.Count <= 0) return;//未設定時
-        for (int LP = 0; LP < i_num; LP++)
-        {
-            int x = (int)Random.Range(0, list.Count);
-            if (list[x] == null) continue;
-            Instantiate(list[x],//
-                    new Vector3(Random.Range(-X - Xs, X + Xs), Random.Range(-Y - Ys, Y + Ys), Random.Range(-Z - Zs, Z - Zs)),//ここの値を変えれば乱数でゴミ出ますが電池の都合上できなかったのでだれかお願いします
-                     Quaternion.Euler(0f, 0f, 0f));
-            Debug.Log("unko");
-        }
+
+        gameObject.transform.position = new Vector3(x, y, z);
+        gameObject.transform.localScale = new Vector3(xs, ys, Zs);
+
     }
 }
