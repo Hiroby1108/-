499f9fd945453048cca80e92780be29c75f276ba
Inomaruti <22inomata.kouki@gmail.com>
2019-02-19 11:19:16 +0900

    わきっちのやつ追加

---
+++ Assets/Wakis/TrackerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackerController : MonoBehaviour, IController
+{
+    private TrackerGetter TG;
+
+    public float sensitivity = 0.1f;
+    float x, x_;
+    float y;
+    float m;
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = y = m = 0f;
+        TG = this.GetComponent<TrackerGetter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = TG.getTrackRot().y * 10f;
+        Debug.Log(x);
+        y += TG.getTrackRot().x * sensitivity;
+
+
+
+        if (x < -90f) x = -90f;
+        else if (x > 90f) x = 90f;
+        if (y < -90f) y = -90f;
+        else if (y > 90f) y = 90f;
+
+        m += Input.GetAxis("Mouse ScrollWheel");
+        if (m <= 0f) m = 0f;
+        else if (m > 90f) m = 90f;
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            x = 0f;
+            y = 0f;
+            m = 0f;
+        }
+    }
+    public float rotX()
+    {
+
+        return x;
+    }
+    public float rotY()
+    {
+        return y;
+    }
+    public float openMouth()
+    {
+        return m;
+    }
+    public bool reActive()
+    {
+        if (this.GetComponent<DebugMouseRot>().enabled)
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+}
