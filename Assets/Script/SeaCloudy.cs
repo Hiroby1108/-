8a869fc2f748e18c7f803d0165f1ddcce40ad397
Hiroby1108 <hirobymiya@gmail.com>
2019-02-20 13:19:35 +0900

    海の可視化実装

---
+++ Assets/Script/SeaCloudy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeaCloudy : MonoBehaviour
+{
+    public GameObject Sea;
+
+    byte c = 0;
+    byte d = 180;
+    void Start()
+    {
+    }
+    void Update()
+    {
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Trash")
+        {
+            if (d > 0)
+            {
+                if (c < 250)
+                {
+                    c += 14;
+                    d -= 10;
+                    Debug.Log(c);
+                    Sea.GetComponent<Renderer>().material.color = new Color32(0, 0, c, d);
+                }
+            }
+
+        }
+
+    }
+}
