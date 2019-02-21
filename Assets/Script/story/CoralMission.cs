2f48730f12486bc256f38249aaf9c87992169ac4
Hiroby1108 <hirobymiya@gmail.com>
2019-02-21 18:56:34 +0900

    サンゴ完成＆ファイリングしたよ★

---
+++ Assets/Script/story/CoralMission.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CoralMission : MonoBehaviour
+{
+    [SerializeField, Header("次のシーン名"), Tooltip("未設定でもれなくワキルーム行")]
+    public string NextSceneName;
+    private int i=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Trash")
+        {
+            i++;
+            if (i == 10)
+            {
+                Invoke("SceneChange", 2.0f);
+            }
+        }
+    }
+    void SceneChange()
+    {
+        SceneManager.LoadScene(NextSceneName);
+        Debug.Log(NextSceneName);
+    }
+}
