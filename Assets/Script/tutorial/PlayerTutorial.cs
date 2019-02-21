c75a74251f83344db0a720bab0e72c8d3be3b071
Hiroby1108 <hirobymiya@gmail.com>
2019-02-21 19:33:05 +0900

    ランダムでシーンを出力するやつ

--- Assets/Script/tutorial/PlayerTutorial.cs
+++ Assets/Script/tutorial/PlayerTutorial.cs
@@ -7,21 +7,27 @@ using UnityEngine.SceneManagement;
 public class PlayerTutorial : MonoBehaviour
 {
     Text myText;
-    int i = 0;
-    [SerializeField, Header("次のシーン名"), Tooltip("未設定でもれなくワキルーム行")]
+    private int i = 0;
+    private int x=0;
+
+   [SerializeField, Header("次のシーン名"), Tooltip("未設定でもれなくワキルーム行")]
     public string NextSceneName;
+    [SerializeField, Header("次のシーン名"), Tooltip("未設定でもれなくワキルーム行")]
+    public string NextSceneNameTurtle;
+    [SerializeField, Header("次のシーン名"), Tooltip("未設定でもれなくワキルーム行")]
+    public string NextSceneNameCoral;
     // Start is called before the first frame update
     void Start()
     {
         myText = GameObject.Find("Text").GetComponentInChildren<Text>();
         //NextSceneName = SceneManager.GetActiveScene().name;
-      //  NextSceneName
+        //  NextSceneName
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     void OnCollisionEnter(Collision col)
     {
@@ -31,9 +37,25 @@ public class PlayerTutorial : MonoBehaviour
         }
         if (i == 3)
         {
+            x = Random.Range(1, 4);
+
+            if(x == 1){
             myText.GetComponent<Text>().enabled = true;
             myText.text = "チュートリアルは終了です";
             Invoke("SceneChange", 2.0f);
+            }
+            if (x == 2)
+            {
+                myText.GetComponent<Text>().enabled = true;
+                myText.text = "チュートリアルは終了です";
+                Invoke("SceneChangeTurtle", 2.0f);
+            }
+            if (x == 3)
+            {
+                myText.GetComponent<Text>().enabled = true;
+                myText.text = "チュートリアルは終了です";
+                Invoke("SceneChangeCoral", 2.0f);
+            }
         }
     }
     void SceneChange()
@@ -41,4 +63,15 @@ public class PlayerTutorial : MonoBehaviour
         SceneManager.LoadScene(NextSceneName);
         Debug.Log(NextSceneName);
     }
+    void SceneChangeTurtle()
+    {
+        SceneManager.LoadScene(NextSceneNameTurtle);
+        Debug.Log(NextSceneNameTurtle);
+    }
+    void SceneChangeCoral()
+    {
+        SceneManager.LoadScene(NextSceneNameCoral);
+        Debug.Log(NextSceneNameCoral);
+    }
+
 }
