b9db20a1dc4969f8bebce467d6dcd1e506533960
Kanta-Osawa <kantaism.ds@gmail.com>
2019-02-21 14:55:05 +0900

    花火

---
+++ Assets/Sound/FireworkControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireworkControl : MonoBehaviour
+{
+    public GameObject obj;
+    public AudioSource SE;
+    public AudioClip firework;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource audiosource = gameObject.GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Instantiate(obj);
+            SE.PlayOneShot(firework);
+        }
+    }
+}
