21b8d35bd294e4bf8ae6ad4324fc582c8141ac10
Inomaruti <22inomata.kouki@gmail.com>
2019-02-22 16:16:25 +0900

    リザルトスコア表示（ペットとタバコのみだが）完璧、リザルト配置調整

--- Assets/Script/ResultTextMyTotal.cs
+++ Assets/Script/ResultTextMyTotal.cs
@@ -6,8 +6,10 @@ using UnityEngine.UI;
 
 public class ResultTextMyTotal : MonoBehaviour
 {
-    private int valueP, valueT;
+    private int valueP, valueT,num;
+    private float numf;
     GameObject ResultUI1_1;
+    public static float mytotal=0f,Allmytotal=0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +20,13 @@ public class ResultTextMyTotal : MonoBehaviour
         valueT = PlayeGetItem.getTrashList()["tabako(Clone)"];
         foreach (KeyValuePair<string, int> DictKvp in PlayeGetItem.getTrashList())
         {
-            uitext.text = Convert.ToString((valueP *0.000015)+(valueT*0.03)) + "kg";
+            mytotal = float.Parse(Convert.ToString((valueT * 0.000015f) + (valueP * 0.03f)));
+            num = (int)(mytotal * 100);
+            numf = (float)num / 100f;
+            Debug.Log(num);
+            Debug.Log(numf);
+            uitext.text = numf.ToString();
+            Debug.Log(mytotal);
         }
     }
 
