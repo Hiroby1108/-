e6c9dd03c114668850d9bb5802e6cc868377abd3
Inomaruti <22inomata.kouki@gmail.com>
2019-02-22 18:35:15 +0900

    リザルトのテキスト表示のモデル追加、スコア表示のscript追加

---
+++ Assets/Script/ResultTextGyomou.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultTextGyomou : MonoBehaviour
+{
+    private int value;
+    GameObject ResultUI2_32;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResultUI2_32 = GameObject.Find("ResultText2_32");
+        Text uitext = GetComponent<Text>();
+
+        value = PlayeGetItem.getTrashList()["Gyomou(Clone)"];
+        foreach (KeyValuePair<string, int> DictKvp in PlayeGetItem.getTrashList())
+        {
+            uitext.text = Convert.ToString(value * 5) + "kg";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Script/ResultTextTaiya.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultTextTaiya : MonoBehaviour
+{
+    private int value;
+    GameObject ResultUI2_42;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResultUI2_42 = GameObject.Find("ResultText2_42");
+        Text uitext = GetComponent<Text>();
+
+        value = PlayeGetItem.getTrashList()["Taiya(Clone)"];
+        foreach (KeyValuePair<string, int> DictKvp in PlayeGetItem.getTrashList())
+        {
+            uitext.text = Convert.ToString(value * 18) + "kg";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
