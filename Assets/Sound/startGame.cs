2b0f2174f12c49bf48527490863ecc5fe2937e48
Kanta-Osawa <kantaism.ds@gmail.com>
2019-02-22 16:19:28 +0900

    エフェクト

---
+++ Assets/Sound/startGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startGame : MonoBehaviour
+{
+    public AudioSource SE;
+    public AudioClip start;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource audiosource = gameObject.GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SE.PlayOneShot(start);
+        }
+    }
+    }
