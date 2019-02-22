8c37085dc28bc98d45b2b2b0f7dad94d7813729d
Hiroby1108 <hirobymiya@gmail.com>
2019-02-22 18:45:34 +0900

    訂正

---
+++ Assets/Script/ResultTextTerebi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultTextTerebi : MonoBehaviour
+{
+    private int value;
+    GameObject ResultUI2_52;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResultUI2_52 = GameObject.Find("ResultText2_52");
+        Text uitext = GetComponent<Text>();
+
+        value = PlayeGetItem.getTrashList()["Terebi(Clone)"];
+        foreach (KeyValuePair<string, int> DictKvp in PlayeGetItem.getTrashList())
+        {
+            uitext.text = Convert.ToString(value * 40) + "kg";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
