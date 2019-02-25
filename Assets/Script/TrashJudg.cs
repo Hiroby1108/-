97ebecd2ca3baeb364e4c8d5972dcb642a66c7e4
Inomaruti <22inomata.kouki@gmail.com>
2019-02-25 12:22:48 +0900

    ミッションの成功判定のscript追加

---
+++ Assets/Script/TrashJudg.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TrashJudg : MonoBehaviour
+{
+    [SerializeField, Header("イルカがゴミを食べてもいい数")]
+    public int TrashRecovery;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Iruka")
+        {
+            if (IrukaTuchJudg.Trashnum <= TrashRecovery)
+            {
+                SceneManager.LoadScene("Result"); //ミッションクリアシーンへ移動
+                Debug.Log("成功!!!");
+            }else {
+                SceneManager.LoadScene("result"); //ミッション失敗シーンへ移動
+                Debug.Log("失敗...");
+            }
+            
+        }
+    }
+}
