a5b36290f23b9e79328e7f4adaaf6f4868b4799a
Inomaruti <22inomata.kouki@gmail.com>
2019-02-26 10:45:03 +0900

    リザルトの漁網重さ追加、ウキ追加、ゴミの初期化値追加

---
+++ Assets/Script/ResultTextUki.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultTextUki : MonoBehaviour
+{
+    private int value;
+    GameObject ResultUI2_22;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResultUI2_22 = GameObject.Find("ResultText2_72");
+        Text uitext = GetComponent<Text>();
+
+        value = PlayeGetItem.getTrashList()["Uki(Clone)"];
+        foreach (KeyValuePair<string, int> DictKvp in PlayeGetItem.getTrashList())
+        {
+            uitext.text = Convert.ToString(value * 0.019) + "kg";
+        }
+    }
+}
