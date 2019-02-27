6bc74b9c7df4f2f417f5bdb254ce04372fb0b788
Hiroby1108 <hirobymiya@gmail.com>
2019-02-27 17:27:20 +0900

    修正

---
+++ Assets/Script/FireworkControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireworkControl : MonoBehaviour
+{
+    public GameObject obj;
+    public AudioSource SE;
+    public AudioClip firework;
+    bool test;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource audiosource = gameObject.GetComponent<AudioSource>();
+        test =false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(test == false)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                Instantiate(obj);
+                SE.PlayOneShot(firework);
+                test = true;
+            }
+        }
+    }
+}
