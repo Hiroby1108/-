514ea350557736984933c6886a55605dd106e446
Inomaruti <22inomata.kouki@gmail.com>
2019-04-16 16:25:34 +0900

    リザルトの総合スコア表示に漁網（まっすぐ）とウキを追加、リザルトの合計スコアがおかしくなる不具合を修正（デバッグはしてないです）

--- Assets/Script/Result/ResultAllText.cs
+++ Assets/Script/Result/ResultAllText.cs
@@ -7,34 +7,31 @@ using UnityEngine.UI;
 public class ResultAllText : MonoBehaviour
 {
     private int num;
-    private float numf=0f;
     public float TrashAllTotal = 0;
     GameObject ResultUI1_3;
     // Start is called before the first frame update
     void Start()
     {
         //PlayerPrefs.DeleteKey("SCORE");   //スコア初期化
-        TrashAllTotal = PlayerPrefs.GetFloat("SCORE", 0);
+        TrashAllTotal = PlayerPrefs.GetFloat("SCORE", 0);   //("キー（保存場所）",データがない場合のデフォ値）
 
         ResultUI1_3 = GameObject.Find("ResultText1_3");
         Text uitext = GetComponent<Text>();
         if (TrashAllTotal == 0)
         {
-            //num = (int)(ResultTextMyTotal.mytotal * 10);
-            //numf += ((float)num) / 1000f;
-            //Debug.Log("a" + num);
-            //Debug.Log("a" + numf);
             TrashAllTotal += (ResultTextMyTotal.mytotal / 1000);
             uitext.text = /*(numf)*/ + TrashAllTotal + "";
-            //Debug.Log("a" + TrashAllTotal);
-            //TrashAllTotal += (ResultTextMyTotal.mytotal / 1000);
-            //Debug.Log("a2_" + TrashAllTotal);
+            Debug.Log("a" + TrashAllTotal);
+            OnDestroy();
+            Debug.Log("PlayerPrefs保存完了");
         }
         else
         {
             TrashAllTotal += (ResultTextMyTotal.mytotal / 1000);
             uitext.text = TrashAllTotal + "";
             Debug.Log("b" + TrashAllTotal);
+            OnDestroy();
+            Debug.Log("PlayerPrefs保存完了");
         }
     }
 
@@ -47,7 +44,7 @@ public class ResultAllText : MonoBehaviour
 void OnDestroy()
     {
         // スコアを保存
-        PlayerPrefs.SetFloat("SCORE", TrashAllTotal);
-        PlayerPrefs.Save();
+        PlayerPrefs.SetFloat("SCORE", TrashAllTotal);   //TrashAllTotalの値をSCORE(キー)にセット
+        PlayerPrefs.Save(); //全ての変更内容を保存
     }
 }
